19cf716c96f5516efa872776b96e67157f7d16b5
Jimmy Leta <jimmy.leta@gmail.com>
2022-08-21 20:26:53 -0400

    Changing app title

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,8 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{name}!</h1>
 </main>
 
 <style>
@@ -27,4 +26,5 @@
 			max-width: none;
 		}
 	}
+	
 </style>
